6be409dc6f394c52e08689e824457af09e9ccad1
Jodu555 <Jodu505@gmail.com>
2022-01-09 13:53:32 +0100

    some in and excluding fixes

--- src/backupManager.js
+++ src/backupManager.js
@@ -4,13 +4,15 @@ const path = require('path');
 const partial = (config) => {
     const backupPaths = [];
 
-
+    const matcher = {
+        dir: new RegExp(config.excluding.dirs.join('|'), 'gi'),
+        file: new RegExp(config.excluding.files.join('|'), 'gi'),
+    };
+    // console.log(matcher);
+    // console.log(matcher.dir.test('D:\\Allgemein\\Ich\\Hobbys\\Programmieren\\Web Development\\NodeJS\\AmazonPriceTracker\\node_modules\\concat-map'));
+    // return;
     config.entrys.forEach(entry => {
-
-        let list = getFiles({
-            dir: new RegExp(config.excluding.dirs.join('|'), 'gi'),
-            file: new RegExp(config.excluding.files.join('|'), 'gi'),
-        }, entry);
+        let list = getFiles(matcher, entry);
         console.log(list);
 
     });
@@ -19,19 +21,23 @@ const partial = (config) => {
 const getFiles = (matcher, listPath) => {
     let list = fs.readdirSync(listPath);
     list = list.map(e => path.join(listPath, e));
+    list = list.filter(e => !matcher.dir.test(e) && !matcher.file.test(e))
     list.forEach(elem => {
         try {
             if (fs.statSync(elem).isDirectory()) {
-                if (!matcher.dir.test(elem))
-                    list = list.concat(getFiles(matcher, elem));
+                // if (!matcher.dir.test(elem)) {
+                list = list.concat(getFiles(matcher, elem));
+                // }
             } else {
-                if (!matcher.file.test(elem))
-                    list.push(elem);
+                // if (!matcher.file.test(elem) && !matcher.dir.test(elem)) {
+                list.push(elem);
+                // }
             }
         } catch (error) {
             console.log(error);
         }
     });
+    list = [...new Set(list)];
     return list;
 }
 
