2e896ecdedc48da12860c386828bed6f758569b2
Jodu555 <Jodu505@gmail.com>
2022-01-09 10:40:40 +0100

    Fixed the obvious issues

--- src/backupManager.js
+++ src/backupManager.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const path = require('path');
 
 const partial = (config) => {
     const backupPaths = [];
@@ -15,16 +16,17 @@ const getFiles = (path) => {
     let list = fs.readdirSync(path);
     list.forEach(elem => {
         try {
+            console.log(fs.statSync(path.join(path, elem)).isDirectory());
             if (fs.statSync(path.join(path, elem)).isDirectory()) {
                 list.concat(getFiles(path.join(path, elem)));
             } else {
                 list.push(path.join(elem));
             }
         } catch (error) {
-
+            console.log(error);
         }
     });
-    return files;
+    return list;
 }
 
 
