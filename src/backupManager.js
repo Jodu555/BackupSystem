b8c70832877bb1f9d98e85cbf0373806e7c5f2d4
Jodu555 <Jodu505@gmail.com>
2022-01-09 10:54:00 +0100

    Pushes files with their path

--- src/backupManager.js
+++ src/backupManager.js
@@ -16,11 +16,10 @@ const getFiles = (listPath) => {
     let list = fs.readdirSync(listPath);
     list.forEach(elem => {
         try {
-            console.log(fs.statSync(path.join(listPath, elem)).isDirectory());
             if (fs.statSync(path.join(listPath, elem)).isDirectory()) {
                 list = list.concat(getFiles(path.join(listPath, elem)));
             } else {
-                list.push(path.join(elem));
+                list.push(path.join(listPath, elem));
             }
         } catch (error) {
             console.log(error);
